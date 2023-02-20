5b64d7835ade1041c395af581a54a3ac53ab9cf7
Ryan Goh <ryangohce@gmail.com>
2023-02-20 15:58:07 +0800

    hash user password

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import { Prisma } from "@prisma/client"
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import bcrypt from "bcryptjs"
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+//Filter objects in response
 function filter(obj, ...keys){
   return keys.reduce((a,c) => ({ ...a, [c]: obj[c]}), {})
+}
+
+function validateUser(input) {
+  const validationErrors = {}
+
+  if (!('name' in input) || input['name'].length == 0){
+    validationErrors['name'] = 'cannot be blank'
+  }
+
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  }
+
+  if ('password' in input && input['password'].length < 8) {
+    validationErrors['password'] = 'should be at least 8 characters'
+  }
+
+  if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  return validationErrors 
 }
 
 app.get('/', async (req,res) => {
@@ -18,6 +46,14 @@ app.get('/', async (req,res) => {
 
 app.post('/users', async(req,res) => {
   const data = req.body
+
+  const validationErrors = validateUser(data)
+
+  data.password = bcrypt.hashSync(data.password,8)
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
+  })
 
   prisma.user.create({ 
     data 
