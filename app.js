c3163fba7827a163375c2fb23cd53ac5f304434f
Ryan Goh <ryangohce@gmail.com>
2023-02-24 10:48:00 +0800

    installed morgan for logging

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express"
 import cors from 'cors'
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
+import morgan from "morgan"
 
 const app = express()
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
 
