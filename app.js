b431494760b313a7015408d62e21ad3524ba0507
Ryan Goh <ryangohce@gmail.com>
2023-02-27 17:33:54 +0800

    added middleware for token authorization

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
 import morgan from "morgan"
+import auth from "./src/middlewares/auth.js"
 
 const app = express()
 app.use(morgan('combined'))
@@ -11,5 +12,9 @@ app.use(cors())
 
 app.use('/users', userRouter)
 app.use('/auth', authRouter)
+
+app.get('/protected', auth, (req,res) => {
+  res.json({"hello": "world"})
+})
 
 export default app
