89f2214a54f9f677c0cf0ad228049bb61ab6300a
Ryan Goh <ryangohce@gmail.com>
2023-03-03 14:58:56 +0800

    Implement more efficient way to convert float to int, removed error handling since frontend already handles it.

--- src/controllers/images.controllers.js
+++ src/controllers/images.controllers.js
@@ -1,9 +1,6 @@
 import express from 'express'
 import prisma from '../utils/prisma.js'
 import auth from '../middlewares/auth.js'
-import { validateImage } from '../validators/image.js'
-
-
 
 const router = express.Router()
 
@@ -11,22 +8,7 @@ router.post('/',auth, async (req, res) => {
   let data = req.body
 
   //convert image price from float to int, add 00 to price without decimal place
-  if (data.price.includes(".")){
-    let arr = data.price.split(".")
-    data.price = arr.join("")
-  } else{
-    data.price = data.price + "00"
-  }
-
-  //convert price from string to number
-  data.price = +data.price
-
-  const validationErrors = validateImage(data)
-  
-  if (Object.keys(validationErrors).length != 0) return
-  res.status(400).send({
-    error: validationErrors
-  })
+  data.price = parseInt(parseFloat(data.price)*100)
 
   prisma.images.create({
     data: {
