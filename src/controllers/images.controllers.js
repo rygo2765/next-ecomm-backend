458419e86ac0ff978eb06e3024b6360f330e1ddd
Ryan Goh <ryangohce@gmail.com>
2023-03-03 15:47:26 +0800

    implement GET/images enpoint to display images in website

--- src/controllers/images.controllers.js
+++ src/controllers/images.controllers.js
@@ -1,23 +1,11 @@
 import express from 'express'
 import prisma from '../utils/prisma.js'
-import auth from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.post('/',auth, async (req, res) => {
-  let data = req.body
-
-  //convert image price from float to int, add 00 to price without decimal place
-  data.price = parseInt(parseFloat(data.price)*100)
-
-  prisma.images.create({
-    data: {
-      ...data,
-      ownerId: req.user.payload.id
-    }
-  }).then(images => {
-    return res.json(images)
-  })
+router.get('/', async (req, res) => {
+  const allImages = await prisma.images.findMany()
+  return res.json(allImages)
 })
 
 export default router
